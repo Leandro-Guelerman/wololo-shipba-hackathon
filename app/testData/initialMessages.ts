33f3138b9a587c13ac2bb655cc910b266f89c0ad
Gabriel Villoldo <gabrielvilloldo@gmail.com>
2025-03-09 08:38:50 -0300

    fix: wording

--- app/testData/initialMessages.ts
+++ app/testData/initialMessages.ts
@@ -5,7 +5,7 @@ export const initialMessages: Message[] = [
         id: '1',
         text: `🫶 ¡Hola! Vamos a ayudarte a reservar las mejores vacaciones de tu vida. Por favor indicanos a dónde te gustaría viajar.
         
-"Quiero ir a Puerto Iguazú en septiembre"
+"Quiero ir a Puerto Iguazú 15 días en octubre"
 "Quiero ir de Madrid a Londres en noviembre"`,
     }
 ];
