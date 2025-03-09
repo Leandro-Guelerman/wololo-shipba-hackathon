8d5366e5ef1826c94b9c667d50fdc1d924b69e67
Gabriel Villoldo <gabrielvilloldo@gmail.com>
2025-03-09 05:40:54 -0300

    fix: loading

--- app/components/ChatLoader.tsx
+++ app/components/ChatLoader.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 export const ChatLoader: React.FC = () => {
     return (
-        <div className="flex items-center space-x-2 p-4 max-w-[200px] bg-gray-100 rounded-2xl rounded-bl-none">
+        <div className="flex items-center space-x-2 p-4 max-w-[60px] mx-auto bg-gray-50 rounded-2xl rounded-bl-none">
             <div className="flex space-x-1">
                 <div className="w-2 h-2 bg-gray-400 rounded-full animate-bounce" style={{ animationDelay: '0ms' }} />
                 <div className="w-2 h-2 bg-gray-400 rounded-full animate-bounce" style={{ animationDelay: '150ms' }} />
@@ -11,4 +11,4 @@ export const ChatLoader: React.FC = () => {
             </div>
         </div>
     );
-}; 
+};
