3c2df329f39544f292769b97f293b8288db9ad43
Gabriel Villoldo <gabrielvilloldo@gmail.com>
2025-03-09 07:28:25 -0300

    fix: wording

--- app/testData/initialMessages.ts
+++ app/testData/initialMessages.ts
@@ -5,6 +5,6 @@ export const initialMessages: Message[] = [
         id: '1',
         text: `🫶 ¡Hola! Vamos a ayudarte a reservar las mejores vacaciones de tu vida. Por favor indicanos a dónde te gustaría viajar.
         
- Por ejemplo: "Quiero ir a ver el Empire State".`,
+ Por ejemplo: "Quiero ir a Madrid desde Londres en Noviembre".`,
     }
 ];
